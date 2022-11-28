bd07f7e13e1f1dd002b5bd69a45a937a9dc69b5e
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-28 01:42:58 -0500

    [fixme] The env is not read, and it generates an error in the connection

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-// TODO Put in environment variable
-const MONGODB_URI = ''; // URL connection
+// FIXME The env is not read, and it generates an error in the connection
+const MONGODB_URI = 'mongodb+srv://userMongoDB:userTestMongoDB@cluster0.qpbcvh5.mongodb.net/?retryWrites=true&w=majority'; // URL connection
+console.log("🚀 ~ file: database.js ~ line 5 ~ MONGODB_URI", MONGODB_URI)
 
 mongoose.connect(MONGODB_URI,{
   useNewUrlParser: true,
