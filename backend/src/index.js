36eada2c0f8079da43e1609c03aa63e827ffcec4
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-28 00:34:50 -0500

    [add] Create user in DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 require('./database');
-
+app.use(express.json());
 app.use('/api',require('./routes/index'));
 
 app.listen(3000);
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
 const router = Router();
+const User = require('../model/User');
 
 router.get('/',(req, res) => {
   res.send("Hello world");
+});
+
+router.post('/signUp', async(req, res) => {
+  const { email, password } = req.body;
+  const newUser = new User({ email, password });
+  await newUser.save();
+  console.log("🚀 ~ file: index.js ~ line 8 ~ router.get ~ newUser", newUser)
+  res.send('Register user')
 });
 
 module.exports = router;
