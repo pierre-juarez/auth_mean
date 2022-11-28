0f7ab4d91f5b92a4a725dbe527543e7f6b6f09df
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-28 01:28:33 -0500

    [add] Authorized routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -27,4 +27,80 @@ router.post('/signIn', async(req, res) => {
   return res.status(200).json({token});
 });
 
+router.get('/tasks',(req, res) => {
+  const dataTasks = [
+    {
+      _id: 1,
+      name: "Task 1",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 2,
+      name: "Task 2",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 3,
+      name: "Task 3",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 4,
+      name: "Task 4",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    }
+  ];
+  res.status(200).json(dataTasks);
+});
+
+router.get('/private-tasks', validateToken, (req, res) => {
+  const dataTasks = [
+    {
+      _id: 1,
+      name: "Task 1",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 2,
+      name: "Task 2",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 3,
+      name: "Task 3",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    },
+    {
+      _id: 4,
+      name: "Task 4",
+      description: "Lorem ipsum",
+      date: "2022-11-28T05:30:29.779+00:00"
+    }
+  ];
+  res.status(200).json(dataTasks);
+});
+
+router.get('/profile',validateToken, (req, res) => {
+  res.send(req.userID);
+})
+
 module.exports = router;
+
+function validateToken(req, res, next){
+  if(!req.headers.authorization) return res.status(401).send("Unauthorized request");
+  
+  const token = req.headers.authorization.split(" ")[1];
+
+  if(typeof token === 'undefined') return res.status(401).send("Unauthorized request");
+  
+  const data = jwt.verify(token, process.env.SECRET);
+  req.userID = data._id;
+  next();
+}
