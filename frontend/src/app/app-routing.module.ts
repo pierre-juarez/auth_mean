c243f13517c8771ffa33b6f48d02f5cfe39e429f
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-29 00:14:18 -0500

    [add] Component routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { PrivateComponent } from './components/private/private.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'private',
+    component: PrivateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
