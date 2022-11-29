c7c183c576f349c3c37bbb8f6b67e3446b266364
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-11-28 23:31:10 -0500

    [add] Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PrivateComponent } from './components/private/private.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    SigninComponent,
+    SignupComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule,
