32ed51fdd2a252bbd0f8beca2d8e9e5b006b7494
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-12-01 00:13:07 -0500

    [add] CORS - Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 require('./database');
+app.use(cors());
 app.use(express.json());
 app.use('/api',require('./routes/index'));
 
