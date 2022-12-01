45ee4ca58018e4b97aca5aa36e790b49850f4a3c
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-12-01 01:07:35 -0500

    [add] Logout

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class AuthService {
 
   private URL = "http://localhost:3000/api"
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
   
   signUp(user: any){
     return this.http.post<any>(this.URL + '/signup', user);
@@ -24,6 +25,11 @@ export class AuthService {
 
   getToken(){
     return localStorage.getItem('token');
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/sigin']);
   }
 
 }
